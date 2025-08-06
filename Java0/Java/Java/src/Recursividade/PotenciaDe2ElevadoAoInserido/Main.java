1e0d67d87e5d840dff2b5418ba227de450eb68b4
GPS_Gui_S <guilherme.perez200@gmail.com>
2025-08-06 18:01:04 -0300

    Update Main.java
    Correção do código (apenas aceitar números naturais).

--- Java0/Java/Java/src/Recursividade/PotenciaDe2ElevadoAoInserido/Main.java
+++ Java0/Java/Java/src/Recursividade/PotenciaDe2ElevadoAoInserido/Main.java
@@ -6,13 +6,15 @@ public class Main {
         int n;
         double potencia = 1;
         int cont = 1;
-        System.out.println("Insira o número inteiro para descobrir o valor da potencía de 2 elevado ao valor indicado: ");
+        System.out.println("Insira o número Natural para descobrir o valor da potencía de 2 elevado ao valor indicado: ");
         n = scanner.nextInt();
 
-        System.out.println("O valor da potência de 2 a este valor é igual a "+potenciaDeDois(n, cont, potencia));
+        //Verificador de natural e 2 elevado ao número indicado
+        System.out.println("O valor da potência de 2 a este valor é igual a "+potenciaDeDois(verificarNatural(n, scanner), cont, potencia));
 
     }
 
+    //Potência de 2 ao número natural indicado
     public static double potenciaDeDois(int n, int cont, double potencia) {
         if (cont <= n) {
             potencia*=2;
@@ -21,4 +23,17 @@ public class Main {
             return potencia;
         }
     }
+
+    public static int verificarNatural(int n, Scanner scanner) {
+        if (n < 0) {
+            do {
+                System.out.println("Número inválido insira um número natural: ");
+                n = scanner.nextInt();
+            } while(n<0);
+            return n;
+        } else {
+            return n;
+        }
+    }
+
 }
